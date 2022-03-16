b82521c10a8a363d068004a6d356d92bfe19db51
Pedro Ivo <cinelli.dev@gmail.com>
2022-03-15 21:51:59 -0300

    Layout - Tela de Entrada

---
+++ src/Componentes/Page.js
@@ -0,0 +1,20 @@
+export default function Page() {
+    return (
+        <>
+        <header>
+    
+        </header>
+        <aside>
+            <div class="header-entrada">
+                <img src="./assets/raio.svg"></img>
+                <h1 class="titulo-entrada">ZapRecall</h1>
+            </div>
+            <div class="caixa_iniciar">
+                <div class="caixa_iniciar-recall">
+                    <p class="iniciar-texto">Iniciar Recall!</p>
+                </div>
+            </div>
+        </aside>
+        </>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import reactDom from "react-dom";
+import Page from "./Componentes/Page"
+
+reactDom.render(<Page />, document.querySelector('.root'))
